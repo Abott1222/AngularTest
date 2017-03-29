8966c8047cb451ee96dd885608b4dd5e10379b5e
Alex Bott <Abott1222@gmail.com>
2017-03-29 13:09:47 -0700

    added new module and routing

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -13,7 +13,8 @@ export const appRoutes:Routes = [
   {path:"users/new", component: CreateUserComponent},
   //:id is parameter placeholder where we expect a value to be passed in on route
   {path: "users/:id", component: UserDetailsComponent},
-  {path: "", redirectTo: "/users", pathMatch:"full"}
+  {path: "", redirectTo: "/users", pathMatch:"full"},
+  {path:"user" , loadChildren:"app/user/user.module#UserModule"}
 
 
 
